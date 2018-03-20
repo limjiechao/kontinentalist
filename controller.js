3a9ef4b362a40a3101709ca86cc712de0cf4e4ee
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-21 00:04:11 +0800

    category dropdown form and API `get` request
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

---
+++ controller.js
@@ -0,0 +1,35 @@
+const unirest = require('unirest');
+
+// // Development Environment Only
+// const config = require('./config');
+// const APP_KEY = config.APP_KEY;
+
+const API_URL = 'https://andruxnet-random-famous-quotes.p.mashape.com/';
+const COUNT = 1;
+
+exports.index = (req, res) => {
+	res.render('home');
+}
+
+exports.quote = (req, res) => {
+	let chosen = req.body.category
+	let quote;
+
+	// These code snippets use an open-source library. http://unirest.io/nodejs
+	unirest.get(`${API_URL}?cat=${chosen}&count=${COUNT}`)
+	.header('X-Mashape-Key', APP_KEY)
+	.header('Accept', 'application/json')
+	.end(function (result) {
+
+		console.log('result.status\n', result.status);
+		console.log('result.headers\n', result.headers);
+		console.log('result.body\n', result.body);
+
+		if (result.status == 200) {
+			let quote = result.body
+			res.render('quote', { output: quote })
+		} else if (result.status == 500) {
+			res.render('quote', { output: { quote: result.body.message, category: 'Error', author: `Status Code: ${result.status}` } })
+		}
+	});
+}
